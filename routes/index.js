ad132adc2b5e98e4aa81d1a51b532d795ebf8710
Mostafa ismail ibrahim <asmaylmr117@gmail.com>
2024-07-22 20:09:04 +0300

    index.js

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,8 @@ const addressRoute = require('./addressRoute');
 const couponRoute = require('./couponRoute');
 const cartRoute = require('./cartRoute');
 const orderRoute = require('./orderRoute');
+const slidesRoute = require('./slidesRoute');
+
 
 const mountRoutes = (app) => {
   app.use('/api/v1/categories', categoryRoute);
@@ -24,6 +26,7 @@ const mountRoutes = (app) => {
   app.use('/api/v1/coupons', couponRoute);
   app.use('/api/v1/cart', cartRoute);
   app.use('/api/v1/orders', orderRoute);
+  app.use('/api/v1/slides', slidesRoute);
 };
 
 module.exports = mountRoutes;
